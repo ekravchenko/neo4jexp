45fcd9d35469231b7c3940776b505d5dc87c9042
Yevgen Kravchenko <yevgen.kravchenko@gmail.com>
2022-09-27 01:53:38 +0300

    extra logs

--- src/main/scala/com/testingzone/neo4j/service/PersonService.scala
+++ src/main/scala/com/testingzone/neo4j/service/PersonService.scala
@@ -1,10 +1,18 @@
 package com.testingzone.neo4j.service
 
 import cats.effect.IO
+import cats.implicits.showInterpolator
 import com.testingzone.neo4j.domain.Person
 import com.testingzone.neo4j.repository.PersonRepository
+import org.typelevel.log4cats.slf4j.Slf4jLogger
 
 class PersonService(repository: PersonRepository) {
 
-  def findAll(): IO[Vector[Person]] = repository.findAll()
+  def findAll(): IO[Vector[Person]] =
+    for {
+      logger <- Slf4jLogger.create[IO]
+      _ <- logger.info("Find all persons...")
+      result <- repository.findAll()
+      _ <- logger.info(show"Result: [$result]")
+    } yield result
 }
